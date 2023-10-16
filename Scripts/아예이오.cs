d7389baa1fed789160d62981dbab4a49ce34834a
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-10-16 12:08:26 +0900

    ✨ Feat: 카메라 각도 조절

---
+++ Scripts/아예이오.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 아예이오 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
